17817cd655fe1e7cbf03a4b71de4a0568857686d
Guylherme Sales <guylherme5252@hotmail.com>
2021-06-11 00:24:56 -0300

    correção de erro final

--- vis_tab_produtos.php
+++ vis_tab_produtos.php
@@ -8,17 +8,13 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/992d37b8ec.js" crossorigin="anonymous"></script>
-    
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
-    <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
-    </svg>
 
     <title>Produtos Cadastrados</title>
   </head>
   <body>
     
     <main class="container">
-        <h3><a href="index.php"><i class="bi bi-arrow-left-circle-fill"></i></a>Ínicio</h3>
+        <h3><a href="index.php">Ínicio</a>Ínicio</h3>
         <h1>Tabela de Produtos Cadastrados</h1>
 
         <table class="table">
