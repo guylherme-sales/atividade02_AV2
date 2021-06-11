4c105c53bf08f50d67e586ead3ae31575cbdc237
Guylherme Sales <guylherme5252@hotmail.com>
2021-06-10 21:32:37 -0300

    criação do frontend

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>ATIVIDADE 02 - AV2</title>
+  </head>
+  <body>
+    
+    <h1>Atividade 02 para AV2</h1>
+    <br>
+    <h2>Aluno: Guylherme Vieira Sales de Oliveira</h2>
+    <br><hr>
+    <p>Essa atividade serve para a pratica de back e front relacionados a um banco de dados.</p>
+
+    <h2>Opções para o BD</h2>
+    <a href="cad_produto.html">Cadastrar Produto</a>
+    <br>
+    <a href="vis_tab_produtos.php.">Visualizar Tabela de Produtos</a>
+    <br>
+    <a href="edt_user.php.">Editar Produto</a>
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
---
+++ vis_tab_produtos.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/992d37b8ec.js" crossorigin="anonymous"></script>
+    
+    <title>Produtos Cadastrados</title>
+  </head>
+  <body>
+    
+    <main class="container">
+        <h1>Tabela de Produtos Cadastrados</h1>
+
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Código</th>
+              <th scope="col">Nome do Produto</th>
+              <th scope="col">Quantidade</th>
+              <th scope="col">Opções</th>
+            </tr>
+          </thead>
+          <tbody>
+            
+            <?php include 'crud/read.php';?>
+
+          </tbody>
+        </table>
+    </main>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+  </body>
+</html>
